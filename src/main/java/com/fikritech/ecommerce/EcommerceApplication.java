0fc5a1e9e52279e7089b26e2d81cfbed9ba9439d
FIkri <edwardvijee@gmail.com>
2022-10-05 16:22:58 +0700

    Memindahkan app class dari com.ecommerce.application menjadi com.ecommerce, dan menghapus application package

--- src/main/java/com/fikritech/ecommerce/EcommerceApplication.java
+++ src/main/java/com/fikritech/ecommerce/EcommerceApplication.java
@@ -1,4 +1,4 @@
-package com.fikritech.ecommerce.application;
+package com.fikritech.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
